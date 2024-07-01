c416f51c7a0009334b92e797d2b2fbbc1c773d3c
it.chhaya <it.chhaya@gmail.com>
2024-07-01 10:37:41 +0700

    first commit

---
+++ src/main/java/co/istad/bigdata/stream/BigDataStreamKakfaApplication.java
@@ -0,0 +1,13 @@
+package co.istad.bigdata.stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigDataStreamKakfaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigDataStreamKakfaApplication.class, args);
+    }
+
+}
